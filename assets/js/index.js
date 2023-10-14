03883970a2e57417c5099469bdc8b9bda0ac82aa
daduque <daanduque@utp.edu.co>
2023-10-14 14:02:34 -0500

    working with json inside js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,40 @@
-let menuTitle = document.querySelector('.aside-title').children[0];
+// objeto literal
+const noticias = [
+    {
+        title: 'Nuevos edificios en la ciudad',
+        image : 'buildings',
+        description : 'JSON Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, vel rerum? Numquam labore nihil officia eligendi provident. Maiores dolores minus blanditiis quis repudiandae commodi modi, porro, tempore, asperiores ipsa reiciendis!'
+    },
+    {
+        title: 'Llegó un nuevo servicio a la ciudad',
+        image : 'deliveries',
+        description : 'JSON Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, vel rerum? Numquam labore nihil officia eligendi provident. Maiores dolores minus blanditiis quis repudiandae commodi modi, porro, tempore, asperiores ipsa reiciendis!'
+    },
+    {
+        title: 'El lago solitario',
+        image : 'lonely',
+        description : 'JSON Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, vel rerum? Numquam labore nihil officia eligendi provident. Maiores dolores minus blanditiis quis repudiandae commodi modi, porro, tempore, asperiores ipsa reiciendis!'
+    },
+    {
+        title: 'Escaleras de evacuación',
+        image : 'stairs',
+        description : 'JSON Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, vel rerum? Numquam labore nihil officia eligendi provident. Maiores dolores minus blanditiis quis repudiandae commodi modi, porro, tempore, asperiores ipsa reiciendis!'
+    }
+]
 
-if(menuTitle.innerHTML === 'Menu'){
-    menuTitle.innerHTML = 'Hola';
-}else {
-    menuTitle.innerHTML = 'UTP';
-}
+let news = document.querySelector('#news');
 
-console.log(menuTitle);
+noticias.forEach( (element) => {
+
+    news.innerHTML += `
+    <article class="news">
+
+        <h3>${ element.title }</h3>
+        <img src="./assets/images/${ element.image }.jpg" alt="">
+        <p>${ element.description }</p>
+        <a href="#">Leer más</a>
+
+    </article> 
+`;
+
+})
